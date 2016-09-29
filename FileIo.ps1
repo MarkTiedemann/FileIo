95a29893837150896c81302722f2a27ca5a9fe47
Mark Tiedemann <www.marktiedemann@gmail.com>
2016-09-29 19:07:52 +0200

    Refactor Invoke-WebRequest usage

--- FileIo.ps1
+++ FileIo.ps1
@@ -3,6 +3,12 @@ $env:FILEIO_USER_AGENT = 'FileIo - Simple file.io client for PowerShell (github.
 $env:FILEIO_TIMEOUT_SEC = 5
 $env:FILEIO_PROXY = $null
 
+function Invoke-FileIo ($link, $method, $contentType, $body, $out) 
+{
+    if (!$method) { $method = 'GET' }
+    Invoke-WebRequest $link -Method $method -ContentType $contentType -Body $body -Out $out -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+}
+
 function Upload-FileIo ($file, $expires)
 {
     $filePath = "$pwd\$file"
@@ -28,13 +34,13 @@ Content-Type: {2}
 
     $body = $template -f $boundary, $file, $fileContentType, $fileContent
 
-    $response = Invoke-WebRequest "https://file.io/?expires=$expires" -Method Post -ContentType $contentType -Body $body -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+    $response = Invoke-FileIo "https://file.io/?expires=$expires" -Method POST -ContentType $contentType -Body $body
     ($response | ConvertFrom-Json).link
 }
 
 function Print-FileIo ($link)
 {
-    (Invoke-WebRequest $link -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC  -Proxy $env:FILEIO_PROXY).ToString()
+    (Invoke-FileIo $link).ToString()
 }
 
 function Save-FileIo ($link, $out)
@@ -42,12 +48,12 @@ function Save-FileIo ($link, $out)
     if ($out)
     {
         # save file with new name
-        Invoke-WebRequest $link -OutFile $out -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+        Invoke-FileIo $link -Out $out
     }
     else
     {
         # save file with the original name
-        $response = Invoke-WebRequest $link -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+        $response = Invoke-FileIo $link
         $file = $response.Headers.'Content-Disposition'.Split('=')[-1]
         $filePath = "$pwd\$file"
         $fileEncoding = [System.Text.Encoding]::GetEncoding('iso-8859-1')
