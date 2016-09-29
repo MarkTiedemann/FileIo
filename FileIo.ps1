584cf10dfacbc009b19118343f1b61ac6c510a95
Mark Tiedemann <www.marktiedemann@gmail.com>
2016-09-29 17:14:19 +0200

    Add expiration option for file upload; Add option to save file with original name; Add customization option for user agent, timeout and proxy; Update readme

--- FileIo.ps1
+++ FileIo.ps1
@@ -1,12 +1,20 @@
 
-function Upload-FileIo ($file) 
+$env:FILEIO_USER_AGENT = 'FileIo - Simple file.io client for PowerShell (github.com/MarkTiedemann/FileIo)'
+$env:FILEIO_TIMEOUT_SEC = 5
+$env:FILEIO_PROXY = $null
+
+function Upload-FileIo ($file, $expires)
 {
     $filePath = "$pwd\$file"
     $fileContentType = [System.Web.MimeMapping]::GetMimeMapping($filePath)
     $fileBytes = [System.IO.File]::ReadAllBytes($filePath)
-    $fileContent = [System.Text.Encoding]::GetEncoding('iso-8859-1').GetString($fileBytes)
+    $fileEncoding = [System.Text.Encoding]::GetEncoding('iso-8859-1')
+    $fileContent = $fileEncoding.GetString($fileBytes)
+
+    if (!$expires) { $expires = 14 } # 14 days by default
 
     $boundary = New-Guid
+    $contentType = 'multipart/form-data; boundary="' + $boundary + '"'
 
     $template = @'
 --{0}
@@ -19,17 +27,31 @@ Content-Type: {2}
 '@
 
     $body = $template -f $boundary, $file, $fileContentType, $fileContent
-    $contentType = 'multipart/form-data; boundary="' + $boundary + '"'
-    $response = Invoke-WebRequest https://file.io -Method Post -ContentType $contentType -Body $body
+
+    $response = Invoke-WebRequest "https://file.io/?expires=$expires" -Method Post -ContentType $contentType -Body $body -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
     ($response | ConvertFrom-Json).link
 }
 
-function Print-FileIo ($link) 
+function Print-FileIo ($link)
 {
-    (Invoke-WebRequest $link).ToString()
+    (Invoke-WebRequest $link -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC  -Proxy $env:FILEIO_PROXY).ToString()
 }
 
-function Save-FileIo ($link, $out) 
+function Save-FileIo ($link, $out)
 {
-    Invoke-WebRequest $link -OutFile $out   
+    if ($out)
+    {
+        # save file with new name
+        Invoke-WebRequest $link -OutFile $out -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+    }
+    else
+    {
+        # save file with the original name
+        $response = Invoke-WebRequest $link -UserAgent $env:FILEIO_USER_AGENT -TimeoutSec $env:FILEIO_TIMEOUT_SEC -Proxy $env:FILEIO_PROXY
+        $file = $response.Headers.'Content-Disposition'.Split('=')[-1]
+        $filePath = "$pwd\$file"
+        $fileEncoding = [System.Text.Encoding]::GetEncoding('iso-8859-1')
+        $fileBytes = $fileEncoding.GetBytes($response.ToString())
+        [System.IO.File]::WriteAllBytes($filePath, $fileBytes)
+    }
 }
--- README.md
+++ README.md
@@ -3,26 +3,58 @@
 
 **Simple [file.io](https://file.io) client for PowerShell.**
 
-## Quickstart
+## Usage
 
 ### Upload a file
 
+`Upload-FileIo $file => https://file.io/$id`
 ```powershell
-Upload-FileIo $file
-# => https://file.io/$id
+Upload-FileIo test.txt
+# => https://file.io/r4nd0m
+```
+
+**- with a [custom expiration time](https://www.file.io/#api) (default: `14` days)**
+
+`Upload-FileIo $file -Expires $time => https://file.io/$id`
+```powershell
+Upload-FileIo test.txt -Expires 1w
+# => https://file.io/r4nd0m
 ```
 
 ### Print a file
 
+`Print-FileIo https://file.io/$id => $content`
 ```powershell
-Print-FileIo https://file.io/$id
-# => $content
+Print-FileIo https://file.io/r4nd0m
+# => Hello PowerShell
 ```
 
 ### Save a file
 
+`Save-FileIo https://file.io/$id`
 ```powershell
-Save-FileIo https://file.io/$id -Out $file
+Save-FileIo https://file.io/r4nd0m
+```
+**- with a new name**
+
+`Save-FileIo https://file.io/$id -Out $file`
+```powershell
+Save-FileIo https://file.io/r4nd0m -Out new.txt
+```
+
+#### Customization
+
+After loading the module, you may overwrite some of its default settings:
+
+```powershell
+$env:FILEIO_USER_AGENT = 'My Custom User Agent'
+# default: FileIo - Simple file.io client for PowerShell (github.com/MarkTiedemann/FileIo)
+
+$env:FILEIO_TIMEOUT_SEC = 10
+# default: 5
+
+$env:FILEIO_PROXY = 'http://myproxy.local'
+# default: $null
 ```
 
 ## Download
